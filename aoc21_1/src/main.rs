195cf1a4892d87a75d0374b2a63913efa986584b
Anthony <als872@nau.edu>
2021-12-04 00:47:42 -0700

    Terrible day 1 solution

--- aoc21_1/src/main.rs
+++ aoc21_1/src/main.rs
@@ -4,8 +4,8 @@ const FPS: [&str; 2] = ["./src/data/test.txt", "./src/data/input.txt"];
 
 fn main() {
     for fp in FPS {
-        println!("Result for file at path {} is: {}", fp, do_work_1(fp));
-        println!("Result for file at path {} is: {}", fp, do_work_2(fp));
+        println!("Result 1 for file at path {} is: {}", fp, do_work_1(fp));
+        println!("Result 2 for file at path {} is: {}", fp, do_work_2(fp));
     }
 }
 
@@ -16,6 +16,7 @@ fn do_work_1(fp: &str) -> u32 {
     let mut depths = depths.lines();
 
     let mut times_increased: u32 = 0;
+    // Really need a less gross way of doing this
     let mut prev_depth: u32 = depths
         .next()
         .unwrap()
@@ -41,19 +42,38 @@ fn do_work_2(fp: &str) -> u32 {
     let mut depths = depths.lines();
 
     let mut times_increased: u32 = 0;
-    let mut prev_depth: u32 = depths
+    let mut prev_depth3: u32 = depths
         .next()
         .unwrap()
         .parse()
         .unwrap();
 
+    let mut prev_depth2: u32 = depths
+        .next()
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    let mut prev_depth1: u32 = depths
+        .next()
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    let mut prev_depth_sum = prev_depth3 + prev_depth2 + prev_depth1;
+
     for depth in depths {
-        let depth = depth.parse().unwrap();
-        if depth > prev_depth {
+        let depth: u32 = depth.parse().unwrap();
+        let depth_sum = prev_depth_sum - prev_depth3 + depth;
+
+        if depth_sum > prev_depth_sum {
             times_increased += 1;
         }
 
-        prev_depth = depth;
+        prev_depth_sum = depth_sum;
+        prev_depth3 = prev_depth2;
+        prev_depth2 = prev_depth1;
+        prev_depth1 = depth;
     }
 
     times_increased
