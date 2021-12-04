17abd3eaad5330caf23b0fccbe58a2b0d5d03011
Anthony <als872@nau.edu>
2021-12-04 12:49:27 -0700

    Use autofmt

--- aoc21_1/src/main.rs
+++ aoc21_1/src/main.rs
@@ -4,20 +4,24 @@ const FPS: [&str; 2] = ["./data/test.txt", "./data/input.txt"];
 
 fn main() {
     for fp in FPS {
-        let depths = fs::read_to_string(fp)
-            .expect("Something went wrong reading the file");
+        let depths = fs::read_to_string(fp).expect("Something went wrong reading the file");
 
-        let depths: Vec<u32> = depths
-            .lines()
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect();
+        let depths: Vec<u32> = depths.lines().map(|x| x.parse::<u32>().unwrap()).collect();
 
-        println!("Result 1 for file at path {} is: {}", fp, do_work_1(&depths));
-        println!("Result 2 for file at path {} is: {}", fp, do_work_2(&depths));
+        println!(
+            "Result 1 for file at path {} is: {}",
+            fp,
+            do_work_1(&depths)
+        );
+        println!(
+            "Result 2 for file at path {} is: {}",
+            fp,
+            do_work_2(&depths)
+        );
     }
 }
 
-fn do_work_1(depths: &[u32])-> u32 {
+fn do_work_1(depths: &[u32]) -> u32 {
     let mut times_increased: u32 = 0;
     let mut prev_depth: u32 = depths[0];
 
@@ -39,7 +43,6 @@ fn do_work_2(depths: &[u32]) -> u32 {
 
     for idx in 0..depths.len() - 3 {
         let depth_sum = prev_depth_sum - depths[idx] + depths[idx + 3];
-
         if depth_sum > prev_depth_sum {
             times_increased += 1;
         }
