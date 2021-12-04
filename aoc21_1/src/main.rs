26c9d564a0e7adcc3c48e7c840da261c8b52ce00
Anthony <als872@nau.edu>
2021-12-04 12:48:02 -0700

    Cleanup day 1

--- aoc21_1/src/main.rs
+++ aoc21_1/src/main.rs
@@ -4,27 +4,25 @@ const FPS: [&str; 2] = ["./data/test.txt", "./data/input.txt"];
 
 fn main() {
     for fp in FPS {
-        println!("Result 1 for file at path {} is: {}", fp, do_work_1(fp));
-        println!("Result 2 for file at path {} is: {}", fp, do_work_2(fp));
+        let depths = fs::read_to_string(fp)
+            .expect("Something went wrong reading the file");
+
+        let depths: Vec<u32> = depths
+            .lines()
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        println!("Result 1 for file at path {} is: {}", fp, do_work_1(&depths));
+        println!("Result 2 for file at path {} is: {}", fp, do_work_2(&depths));
     }
 }
 
-fn do_work_1(fp: &str) -> u32 {
-    let depths = fs::read_to_string(fp)
-        .expect("Something went wrong reading the file");
-
-    let mut depths = depths.lines();
-
+fn do_work_1(depths: &[u32])-> u32 {
     let mut times_increased: u32 = 0;
-    // Really need a less gross way of doing this
-    let mut prev_depth: u32 = depths
-        .next()
-        .unwrap()
-        .parse()
-        .unwrap();
+    let mut prev_depth: u32 = depths[0];
 
-    for depth in depths {
-        let depth = depth.parse().unwrap();
+    for depth in depths[1..].iter() {
+        let depth = *depth;
         if depth > prev_depth {
             times_increased += 1;
         }
@@ -35,45 +33,18 @@ fn do_work_1(fp: &str) -> u32 {
     times_increased
 }
 
-fn do_work_2(fp: &str) -> u32 {
-    let depths = fs::read_to_string(fp)
-        .expect("Something went wrong reading the file");
-
-    let mut depths = depths.lines();
-
+fn do_work_2(depths: &[u32]) -> u32 {
     let mut times_increased: u32 = 0;
-    let mut prev_depth3: u32 = depths
-        .next()
-        .unwrap()
-        .parse()
-        .unwrap();
+    let mut prev_depth_sum: u32 = depths[0..3].iter().sum();
 
-    let mut prev_depth2: u32 = depths
-        .next()
-        .unwrap()
-        .parse()
-        .unwrap();
-
-    let mut prev_depth1: u32 = depths
-        .next()
-        .unwrap()
-        .parse()
-        .unwrap();
-
-    let mut prev_depth_sum = prev_depth3 + prev_depth2 + prev_depth1;
-
-    for depth in depths {
-        let depth: u32 = depth.parse().unwrap();
-        let depth_sum = prev_depth_sum - prev_depth3 + depth;
+    for idx in 0..depths.len() - 3 {
+        let depth_sum = prev_depth_sum - depths[idx] + depths[idx + 3];
 
         if depth_sum > prev_depth_sum {
             times_increased += 1;
         }
 
         prev_depth_sum = depth_sum;
-        prev_depth3 = prev_depth2;
-        prev_depth2 = prev_depth1;
-        prev_depth1 = depth;
     }
 
     times_increased
