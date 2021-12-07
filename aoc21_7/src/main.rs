dd41c1faefcf864f034fb9844deef253e21d78dc
Anthony <als872@nau.edu>
2021-12-06 23:29:49 -0700

    refactor

--- aoc21_7/src/main.rs
+++ aoc21_7/src/main.rs
@@ -2,6 +2,11 @@ use ferris_says::say;
 use std::fs;
 use std::io::{stdout, BufWriter};
 
+enum Part {
+    One,
+    Two,
+}
+
 const FPS: [&str; 2] = ["./data/test.txt", "./data/input.txt"];
 
 fn main() {
@@ -15,38 +20,32 @@ fn main() {
         let message1 = format!(
             "Result 1 for file at path {} is {}",
             fp,
-            do_work1(&crabs, *max_dist)
+            do_work(&crabs, *max_dist, Part::One)
         );
         say(message1.as_bytes(), message1.chars().count(), &mut writer).unwrap();
 
         let message2 = format!(
             "Result 2 for file at path {} is {}",
             fp,
-            do_work2(&crabs, *max_dist)
+            do_work(&crabs, *max_dist, Part::Two)
         );
         say(message2.as_bytes(), message2.chars().count(), &mut writer).unwrap();
     }
 }
 
-fn do_work1(crabs: &[usize], max_dist: usize) -> usize {
-    let mut dist_sums: Vec<usize> = vec![0; max_dist];
-
-    for crab in crabs {
-        for pos in 0..max_dist {
-            dist_sums[pos] += (*crab as isize - pos as isize).abs() as usize;
-        }
-    }
-
-    *dist_sums.iter().min().unwrap()
-}
-
-fn do_work2(crabs: &[usize], max_dist: usize) -> usize {
+fn do_work(crabs: &[usize], max_dist: usize, part: Part) -> usize {
     let mut dist_sums: Vec<usize> = vec![0; max_dist];
 
     for crab in crabs {
         for pos in 0..max_dist {
             let num_steps = (*crab as isize - pos as isize).abs() as usize;
-            dist_sums[pos] += ((num_steps + 1) as f32 * (num_steps as f32 / 2.0)) as usize;
+
+            match part {
+                Part::One => dist_sums[pos] += num_steps,
+                Part::Two => {
+                    dist_sums[pos] += ((num_steps + 1) as f32 * (num_steps as f32 / 2.0)) as usize
+                }
+            }
         }
     }
 
