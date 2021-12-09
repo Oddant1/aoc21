1dc3d5699e650654c5d2a600e9468747fc29f0c1
Anthony <als872@nau.edu>
2021-12-08 17:34:48 -0700

    Gross Day 8

--- aoc21_8/src/main.rs
+++ aoc21_8/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
             let mut input: Vec<&str> = Vec::new();
             let mut output: Vec<&str> = Vec::new();
 
-            let mut line = line.split("|");
+            let mut line = line.split(" | ");
 
             for i in line.next().unwrap().split(" ") {
                 input.push(i);
@@ -33,6 +33,12 @@ fn main() {
             fp,
             do_work1(&outputs)
         );
+
+        println!(
+            "Result 2 for file at path {} is {}",
+            fp,
+            do_work2(&inputs, &outputs)
+        );
     }
 }
 
@@ -40,8 +46,8 @@ fn do_work1(outputs: &[Vec<&str>]) -> usize {
     let mut unique_counts = 0;
 
     for output in outputs {
-        for num in output {
-            let len = num.len();
+        for signal in output {
+            let len = signal.len();
 
             if len == 2 || len == 3 || len == 4 || len == 7 {
                 unique_counts += 1;
@@ -50,4 +56,85 @@ fn do_work1(outputs: &[Vec<&str>]) -> usize {
     }
 
     unique_counts
+}
+
+fn do_work2(inputs: &[Vec<&str>], outputs: &[Vec<&str>]) -> usize {
+    let mut sum = 0;
+
+    for (i, input) in inputs.iter().enumerate() {
+        let mut mappings: [&str; 10] = [""; 10];
+
+        for signal in input.iter().chain(&outputs[i]) {
+            let len = signal.len();
+
+            if len == 2 {
+                mappings[1] = signal;
+            } else if len == 3 {
+                mappings[7] = signal;
+            } else if len == 4 {
+                mappings[4] = signal;
+            } else if len == 7 {
+                mappings[8] = signal;
+            }
+        }
+
+        for signal in input.iter().chain(&outputs[i]) {
+            if diff(mappings[8], signal) == 1 {
+                if diff(mappings[4], signal) == 0 {
+                    mappings[9] = signal;
+                } else if diff(mappings[7], signal) == 0 {
+                    mappings[0] = signal;
+                } else if diff(mappings[1], signal) == 1 {
+                    mappings[6] = signal;
+                }
+            } else if diff(mappings[4], signal) == 1 {
+                if diff(mappings[7], signal) == 0 {
+                    mappings[3] = signal;
+                } else if diff(mappings[7], signal) == 1 {
+                    mappings[5] = signal;
+                }
+            } else if diff(mappings[4], signal) == 2 && diff(mappings[1], signal) == 1 {
+                mappings[2] = signal
+            }
+        }
+
+        sum += calc_value(&outputs[i], mappings);
+    }
+
+    sum
+}
+
+fn diff(str1: &str, str2: &str) -> usize {
+    let mut diff = str1.len();
+
+    'outer: for c1 in str1.chars() {
+        for c2 in str2.chars() {
+            if c1 == c2 {
+                diff -= 1;
+                continue 'outer;
+            }
+        }
+    }
+
+    diff
+}
+
+fn calc_value(outputs: &[&str], mappings: [&str; 10]) -> usize {
+    let mut mult = 1000;
+    let mut val = 0;
+
+    'outer: for output in outputs {
+        for (i, mapping) in mappings.iter().enumerate() {
+            let first = if output.len() >= mapping.len() { output } else { mapping };
+            let second = if output.len() < mapping.len() { output } else { mapping };
+
+            if diff(first, second) == 0 {
+                val += i * mult;
+                mult /= 10;
+                continue 'outer;
+            }
+        }
+    }
+
+    val
 }
